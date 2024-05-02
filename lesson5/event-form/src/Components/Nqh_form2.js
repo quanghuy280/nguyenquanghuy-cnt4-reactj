ec66e0402f9076a6ee80a541d3f8beb695465534
quanghuy280 <166978827+quanghuy280@users.noreply.github.com>
2024-05-02 20:17:42 +0700

    Update and rename Nvh_form2.js to Nqh_form2.js

--- lesson5/event-form/src/Components/Nqh_form2.js
+++ lesson5/event-form/src/Components/Nqh_form2.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 
-class Nvh_form2 extends Component {
+class Nqh_form2 extends Component {
     // 1.
     constructor (props) {
         super(props);
         this.state = {
-           course: "Nvh-HTML5"
+           course: "Nqh-HTML5"
         }
     }    
     //3. hàm xú lý sự kiện
-    NvhHandleChange = (event)=>{
+    NqhHandleChange = (event)=>{
         // cập nhật state
         this.setState({
             course: event.target.value
         })
     }
     //4.
-    NvhhandleSubmit = (event)=>{
+    NqhhandleSubmit = (event)=>{
         event.preventDefault();
         alert(this.state.course)
     }
@@ -25,14 +25,14 @@ class Nvh_form2 extends Component {
         //2.
         return (
             <div>
-                <form onSubmit={this.NvhhandleSubmit}>
+                <form onSubmit={this.NqhhandleSubmit}>
                     <label>Course</label>
                     <select value={this.state.course}
-                            onChange={this.NvhHandleChange}>
-                        <option value={"NvH-HTML5"}>Nvh-HTML5</option>
-                        <option value={"NvH-CSS3"}>Nvh-CSS3</option>
-                        <option value={"NvH-JS"}>Nvh-JS</option>
-                        <option value={"NvH-RJ"}>Nvh-ReactJs</option>
+                            onChange={this.NqhHandleChange}>
+                        <option value={"Nqh-HTML5"}>Nqh-HTML5</option>
+                        <option value={"Nqh-CSS3"}>Nqh-CSS3</option>
+                        <option value={"Nqh-JS"}>Nqh-JS</option>
+                        <option value={"Nqh-RJ"}>Nqh-ReactJs</option>
                     </select>
                     <input type='Submit' value={"Submit"} />
                 </form>
@@ -41,4 +41,4 @@ class Nvh_form2 extends Component {
     }
 }
 
-export default Nvh_form2;
+export default Nqh_form2;
